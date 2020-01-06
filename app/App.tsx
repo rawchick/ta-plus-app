35bf51f3b79e5deb8424c946324f0a5dc1edbc40
bpdevapp <bpdevapp@banpugroup.onmicrosoft.com>
2020-01-06 03:32:09 +0000

    Updated App.tsx

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Navigator from "../app/navigations/Navigator";
 import configureStore from './redux/Store';
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"; //test
 const store = configureStore();
 
 export default class App extends React.PureComponent {
